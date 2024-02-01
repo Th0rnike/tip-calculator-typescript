a183712505a313d8984bacfef060cdae2e740866
Bald Eagle <tsereteli.t02@gmail.com>
2024-02-01 18:20:03 +0400

    updating tips - DONE

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 // import logo from "./assets/logo.svg";
 
 function App() {
-  const [bill, setBill] = useState<number>(0);
-  const [people, setPeople] = useState<number>(0);
+  const [bill, setBill] = useState<number>(200);
+  const [people, setPeople] = useState<number>(2);
   const [tip, setTip] = useState<number>(0);
-  const [custom, setCustom] = useState<number>(0);
+  const [customValue, setCustomValue] = useState<number>(0);
 
-  const tipValue = Number((((bill / people) * (tip || custom)) / 100).toFixed(2));
+  const chooseTipValue = customValue !== 0 ? customValue : tip;
+  const tipValue = Number(
+    (((bill / people) * chooseTipValue) / 100).toFixed(2)
+  );
+  console.log(tipValue);
+
   const totalPrice = Number((bill / people).toFixed(2));
-
   const tipsArray = [5, 10, 15, 25, 50];
 
   const updatePeople = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -22,20 +26,14 @@ function App() {
     const target = e.target as HTMLButtonElement;
     const val = Number(target.textContent?.split("%")[0]);
     setTip(val);
+    setCustomValue(0)
   };
 
-  // const customTip = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   const val = e.target.valueAsNumber;
-  //   setCustom(val);
-  // };
+  const customTip = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const val = e.target.valueAsNumber;
+    setCustomValue(val);
+  };
 
-  // const checkError = () => {
-  //   console.log("chec error run");
-  //   if (people === 0) {
-  //     console.log("settin check error true");
-  //     setError(true);
-  //   }
-  // };
   const handleKeyDown: React.KeyboardEventHandler<HTMLInputElement> = (e) => {
     if (e.key === ".") {
       e.preventDefault();
@@ -73,9 +71,9 @@ function App() {
           className="button"
           type="number"
           placeholder="Custom"
-          onChange={updateTip}
+          onChange={customTip}
           onKeyDown={handleKeyDown}
-          value={custom ? custom : ""}
+          value={customValue ? customValue : ""}
         />
       </div>
       <div>
@@ -88,9 +86,7 @@ function App() {
           value={people ? people : ""}
         />
       </div>
-      <div>
-        Tip Amount {bill && (tipValue || custom) && people ? tipValue : ""}
-      </div>
+      <div>Tip Amount {bill && chooseTipValue && people ? tipValue : ""}</div>
       <div>Total {bill && people ? totalPrice : ""}</div>
       <button onClick={reset}>reset</button>
     </div>
