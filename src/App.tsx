aab5b67675a4e1398659476ca3c45877554857ff
Bald Eagle <tsereteli.t02@gmail.com>
2024-02-02 18:30:07 +0400

    error effects are completed and input css is ready. next step - results css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-// import logo from "./assets/logo.svg";
+import logo from "./assets/logo.svg";
 
 function App() {
   const [bill, setBill] = useState<number>(0);
-  const [people, setPeople] = useState<number>(2);
+  const [people, setPeople] = useState<number>(0);
   const [tip, setTip] = useState<number>(0);
   const [customValue, setCustomValue] = useState<number>(0);
   const [billError, setBillError] = useState<boolean>(false);
+  const [customError, setCustomError] = useState<boolean>(false);
   const [peopleError, setPeopleError] = useState<boolean>(false);
 
   const chooseTipValue = customValue !== 0 ? customValue : tip;
@@ -35,36 +36,77 @@ function App() {
     setCustomValue(val);
   };
 
-  const handleErrorKeys: React.KeyboardEventHandler<HTMLInputElement> = (e) => {
+  const errorForBill: React.KeyboardEventHandler = (e) => {
     if (e.key >= "a" && e.key <= "z") {
       e.preventDefault();
       setBillError(true);
+      setTimeout(() => setBillError(false), 400);
     }
     if (e.key >= "!" && e.key <= ")") {
       e.preventDefault();
       setBillError(true);
+      setTimeout(() => setBillError(false), 400);
     }
     if (e.key === "-" || e.key === "+") {
       e.preventDefault();
       setBillError(true);
+      setTimeout(() => setBillError(false), 400);
     }
   };
 
   const handleKeyDown: React.KeyboardEventHandler<HTMLInputElement> = (e) => {
     if (e.key === ".") {
       e.preventDefault();
+      setPeopleError(true);
+      setTimeout(() => {
+        setPeopleError(false);
+      }, 400);
     }
     if (e.key >= "a" && e.key <= "z") {
       e.preventDefault();
       setPeopleError(true);
+      setTimeout(() => {
+        setPeopleError(false);
+      }, 400);
     }
     if (e.key >= "!" && e.key <= ")") {
       e.preventDefault();
       setPeopleError(true);
+      setTimeout(() => {
+        setPeopleError(false);
+      }, 400);
     }
     if (e.key === "-" || e.key === "+") {
       e.preventDefault();
       setPeopleError(true);
+      setTimeout(() => {
+        setPeopleError(false);
+      }, 400);
+    }
+  };
+
+  const handleCustomError: React.KeyboardEventHandler<HTMLInputElement> = (
+    e
+  ) => {
+    if (e.key === ".") {
+      e.preventDefault();
+      setCustomError(true);
+      setTimeout(() => setCustomError(false), 1000);
+    }
+    if (e.key >= "a" && e.key <= "z") {
+      e.preventDefault();
+      setCustomError(true);
+      setTimeout(() => setCustomError(false), 1000);
+    }
+    if (e.key >= "!" && e.key <= ")") {
+      e.preventDefault();
+      setCustomError(true);
+      setTimeout(() => setCustomError(false), 1000);
+    }
+    if (e.key === "-" || e.key === "+") {
+      e.preventDefault();
+      setCustomError(true);
+      setTimeout(() => setCustomError(false), 1000);
     }
   };
 
@@ -74,52 +116,69 @@ function App() {
     setTip(0);
   };
 
-  // console.log("render");
+  // useEffect
 
   return (
-    <div>
-      <div>
-        {billError ? <span className="error">Numbers Only</span> : ""}
-        <label htmlFor="bill">Bill</label>
-        <input
-          type="number"
-          id="bill"
-          placeholder="0"
-          onKeyDown={handleErrorKeys}
-          onChange={(e) => setBill(e.target.valueAsNumber)}
-          value={bill ? bill : ""}
-        />
+    <div className="App">
+      <img className="image" src={logo} alt="" />
+      <div className="container">
+        <div className="input-div">
+          <div className="input-container">
+            {
+              <span data-value={billError ? "error" : "invicible"}>
+                Numbers Only
+              </span>
+            }
+            <label htmlFor="bill">Bill</label>
+            <input
+              data-borderBill={billError ? "error" : "invicible"}
+              type="number"
+              id="bill"
+              placeholder="0"
+              onKeyDown={errorForBill}
+              onChange={(e) => setBill(e.target.valueAsNumber)}
+              value={bill ? bill : ""}
+            />
+          </div>
+          <div className="buttons-container">
+            <span>Select Tip %</span>
+            {tipsArray.map((t) => (
+              <button key={crypto.randomUUID()} onClick={updateTip}>
+                {t + "%"}
+              </button>
+            ))}
+            <input
+              className={customError ? "button shake" : "button"}
+              type="number"
+              placeholder="Custom"
+              onChange={customTip}
+              onKeyDown={handleCustomError}
+              value={customValue ? customValue : ""}
+            />
+          </div>
+          <div className="input-container">
+            <span data-value={peopleError ? "error" : "invicible"}>
+              Numbers Only
+            </span>
+            <label htmlFor="people">Number of People</label>
+            <input
+              data-borderPeople={peopleError ? "error" : "invicible"}
+              type="number"
+              placeholder="0"
+              onChange={updatePeople}
+              onKeyDown={handleKeyDown}
+              value={people ? people : ""}
+            />
+          </div>
+        </div>
+        <div className="results">
+          <div>
+            Tip Amount {bill && chooseTipValue && people ? tipValue : ""}
+          </div>
+          <div>Total {bill && people ? totalPrice : ""}</div>
+          <button onClick={reset}>reset</button>
+        </div>
       </div>
-      <div>
-        <span>Select Tip %</span>
-        {tipsArray.map((t) => (
-          <button key={crypto.randomUUID()} onClick={updateTip}>
-            {t + "%"}
-          </button>
-        ))}
-        <input
-          className="button"
-          type="number"
-          placeholder="Custom"
-          onChange={customTip}
-          onKeyDown={handleKeyDown}
-          value={customValue ? customValue : ""}
-        />
-      </div>
-      <div>
-        {peopleError ? <span className="error">Numbers Only</span> : ""}
-        <label htmlFor="people">Number of People</label>
-        <input
-          type="number"
-          placeholder="0"
-          onChange={updatePeople}
-          onKeyDown={handleKeyDown}
-          value={people ? people : ""}
-        />
-      </div>
-      <div>Tip Amount {bill && chooseTipValue && people ? tipValue : ""}</div>
-      <div>Total {bill && people ? totalPrice : ""}</div>
-      <button onClick={reset}>reset</button>
     </div>
   );
 }
